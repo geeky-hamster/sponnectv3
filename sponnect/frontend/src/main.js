5f371ba213f3c4eac75fe106783e0734bcf71398
Soham <sknsk1125@gmail.com>
2025-04-29 20:15:13 +0530

    Update frontend dependencies and styles: Add 'sass' as a development dependency in package.json, update package-lock.json with new dependencies including 'sass' and '@parcel/watcher', and import 'master.css' in main.js for enhanced styling.

--- sponnect/frontend/src/main.js
+++ sponnect/frontend/src/main.js
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import './assets/main.css'
+import './assets/master.css'
 
 // Set Indian locale for internationalization
 const indiaLocale = 'en-IN';
